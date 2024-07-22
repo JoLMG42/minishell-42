2b4a37abe9350effc6fd8fb60f1856ef8dd1756f
Julien Sarda <jsarda@bess-f2r1s15.clusters.42paris.fr>
2024-07-22 18:35:03 +0200

    push but still need to fix exit function

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,11 +6,36 @@
 /*   By: jsarda <jsarda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 10:54:23 by jsarda            #+#    #+#             */
-/*   Updated: 2024/07/22 17:01:44 by jsarda           ###   ########.fr       */
+/*   Updated: 2024/07/22 18:34:17 by jsarda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+unsigned long long int	ft_atol(const char *str)
+{
+	int						i;
+	int						sign;
+	unsigned long long int	nb;
+
+	i = 0;
+	sign = 1;
+	nb = 0;
+	while ((str[i] >= 8 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9' && nb < LONG_MAX)
+	{
+		nb = nb * 10 + (str[i] - 48);
+		i++;
+	}
+	return (nb * sign);
+}
 
 void	print_exit_message(char *message, char *args)
 {
@@ -37,9 +62,33 @@ int	ft_isdigit_str(char *c)
 	return (0);
 }
 
+int	ft_strslen_skip(char *args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i] == ' ' || args[i] == '-' || args[i] == '+')
+		i++;
+	i = ft_strlen(args) - i;
+	printf("i = %d\n", i);
+	return (i);
+}
+
+int	check_args(char *args)
+{
+	size_t	arg_size;
+
+	arg_size = ft_strslen_skip(args);
+	if (arg_size > 19)
+	{
+		print_exit_message(": numeric argument required", args);
+		return (1);
+	}
+	return (0);
+}
+
 void	ft_exit(t_data *data, t_shell *shell, char **args)
 {
-	(void)args;
 	if (count_args(data->args) > 1 && ft_isdigit_str(data->args[1]) == 1)
 	{
 		g_return_satus = 2;
@@ -53,8 +102,25 @@ void	ft_exit(t_data *data, t_shell *shell, char **args)
 		print_exit_message(": too many arguments", NULL);
 		return ;
 	}
-	if (args[1])
-		g_return_satus = ft_atoi(args[1]);
+	if (args[1] && check_args(args[1]))
+	{
+		g_return_satus = ft_atol(args[1]);
+		free_child(data, shell, g_return_satus);
+		return ;
+	}
+	if (args[1] && ft_atol(args[1]) <= LONG_MAX)
+	{
+		g_return_satus = ft_atol(args[1]);
+		free_child(data, shell, g_return_satus);
+		return ;
+	}
+	else if (args[1] && ft_atol(args[1]) > LONG_MAX)
+	{
+		print_exit_message(": numeric argument required", args[1]);
+		g_return_satus = ft_atol(args[1]);
+		free_child(data, shell, g_return_satus);
+		return ;
+	}
 	printf("exit\n");
 	free_child(data, shell, g_return_satus);
 }
